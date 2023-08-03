8ca829f19fda324a8abe7ca1d3da6e3185c91ee6
Imran Hossain <blur-imran@hotmail.com>
2023-08-03 17:27:55 +0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module HelloRailsBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:4000', 'http://localhost:4000' # Add your allowed origins here
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
